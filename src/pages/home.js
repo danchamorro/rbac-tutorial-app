a618a94839fcaf69ecc69cbfb9ab9a7461cb0139
danchamorro <dan@danchamorro.com>
2018-12-30 15:24:13 -0500

    rbac-rules

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,24 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 
-const HomePage = () => <h2>Home</h2>;
+import { AuthConsumer } from "../authContext";
+import Login from "../components/Login";
+import PostsList from "../components/PostsList";
+
+const HomePage = () => (
+  <AuthConsumer>
+    {({ authenticated }) =>
+      authenticated ? (
+        <Redirect to="/dashboard" />
+      ) : (
+        <div>
+          <h2>Welcome to React RBAC Tutorial.</h2>
+          <Login />
+          <PostsList />
+        </div>
+      )
+    }
+  </AuthConsumer>
+);
 
 export default HomePage;
